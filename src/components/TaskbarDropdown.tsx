16ff3ffd512bf52dd0d90196b11ec5010b6f8165
ClaytonWas <claywasmund@gmail.com>
2025-01-26 20:46:44 -0500

    better ui.

--- src/components/TaskbarDropdown.tsx
+++ src/components/TaskbarDropdown.tsx
@@ -18,22 +18,28 @@ function TaskbarDropdown({ title, items }: TaskbarDropdownProps) {
     return (
         <div className="relative min-w-16 text-sm text-[var(--text)] m-1 pr-1 md:w-24 md:text-lg cursor-pointer">
             <button
-                className="w-full text-left border border-[var(--accent)] rounded-t bg-[var(--taskbar-indent)] hover:border-[var(--accent)] hover:bg-[var(--taskbar-hover)] pl-2"
+                className={`w-full text-left border border-[var(--accent)] rounded-t bg-[var(--taskbar-indent)] pl-2 ${
+                    isOpen ? "border-b-transparent" : "hover:border-[var(--accent)] hover:bg-[var(--taskbar-hover)]"
+                }`}
                 onClick={() => setIsOpen(!isOpen)}
             >
                 {title}
             </button>
             {isOpen && (
-                <ul className="absolute bg-[var(--taskbar-dropdown-indent)] z-50 shadow-lg text-xs md:text-base border-x border-[var(--accent)]">
+                <ul
+                    className={`absolute bg-[var(--taskbar-dropdown-indent)] z-50 shadow-lg text-xs md:text-base border-x border-[var(--accent)] border-t rounded-b ${
+                        isOpen ? "border-t-[var(--accent)]" : "border-t-transparent"
+                    }`}
+                >
                     {items.map((item, index) => (
                         <li
                             key={index}
                             className="pb-2 md:pb-0 px-1 pr-4 border border-transparent rounded-sm border-b-[var(--accent)] hover:bg-[var(--taskbar-dropdown-indent-hover)] text-nowrap min-w-24"
                             onClick={() => {
                                 if (item.onClick) {
-                                    item.onClick(); // Explicitly call the function
+                                    item.onClick();
                                 }
-                                setIsOpen(false); // Close the dropdown after selection
+                                setIsOpen(false);
                             }}
                         >
                             {item.label}
