ddfb9ec0567928dbb2859074587102a8b2346f23
ClaytonWas <claywasmund@gmail.com>
2025-01-29 09:42:49 -0500

    replit styles.

--- src/components/DockviewExample.tsx
+++ src/components/DockviewExample.tsx
@@ -1,9 +1,9 @@
-import React, { RefObject } from 'react';
+import React, { useRef } from 'react';
 import { DockviewReact, DockviewReadyEvent } from 'dockview';
 
 interface DockviewExampleProps {
-  canvasRef: RefObject<HTMLCanvasElement>;
-  canvasDivRef: RefObject<HTMLDivElement>;
+  canvasRef: React.RefObject<HTMLCanvasElement>;
+  canvasDivRef: React.RefObject<HTMLDivElement>;
   handleMouseDown: React.MouseEventHandler<HTMLDivElement>;
   handleMouseMove: React.MouseEventHandler<HTMLDivElement>;
   handleMouseUpOrLeave: React.MouseEventHandler<HTMLDivElement>;
@@ -17,71 +17,39 @@ const DockviewExample: React.FC<DockviewExampleProps> = ({
   handleMouseUpOrLeave,
 }) => {
   const onReady = (event: DockviewReadyEvent) => {
-    // Add the canvas panel
-    event.api.addPanel({
-      id: 'canvasPanel',
-      component: 'canvasComponent',
-      title: 'Canvas',
-    });
-
-    // Add a filler panel
-    event.api.addPanel({
-      id: 'fillerPanel1',
-      component: 'fillerComponent',
-      title: 'Filler 1',
-    });
-
-    // Add another filler panel
-    event.api.addPanel({
-      id: 'fillerPanel2',
-      component: 'fillerComponent',
-      title: 'Filler 2',
-    });
-
-    // Add yet another filler panel
-    event.api.addPanel({
-      id: 'fillerPanel3',
-      component: 'fillerComponent',
-      title: 'Filler 3',
-    });
+    event.api.addPanel({ id: 'canvasPanel', component: 'canvasComponent', title: 'Canvas' });
+    event.api.addPanel({ id: 'fillerPanel1', component: 'fillerComponent', title: 'Filler 1' });
+    event.api.addPanel({ id: 'fillerPanel2', component: 'fillerComponent', title: 'Filler 2' });
+    event.api.addPanel({ id: 'fillerPanel3', component: 'fillerComponent', title: 'Filler 3' });
   };
 
   return (
     <DockviewReact
       onReady={onReady}
       components={{
-        canvasComponent: () => {
-          return (
+        canvasComponent: () => (
+          <div
+            className="flex-1 flex bg-[var(--canvas-background)] h-full overflow-hidden cursor-grab active:cursor-grabbing"
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUpOrLeave}
+            onMouseLeave={handleMouseUpOrLeave}
+          >
             <div
-              className="flex-1 flex bg-[var(--canvas-background)] h-full overflow-hidden cursor-grab active:cursor-grabbing"
-              onMouseDown={handleMouseDown}
-              onMouseMove={handleMouseMove}
-              onMouseUp={handleMouseUpOrLeave}
-              onMouseLeave={handleMouseUpOrLeave}
+              ref={canvasDivRef}
+              className="relative w-full h-full max-w-full max-h-full flex justify-center items-center"
             >
-              <div
-                ref={canvasDivRef}
-                className="relative w-full h-full max-w-full max-h-full"
-                style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-              >
-                <canvas
-                  ref={canvasRef}
-                  id="imageCanvas"
-                  className="max-w-full max-h-full object-contain"
-                ></canvas>
-              </div>
+              <canvas ref={canvasRef} id="imageCanvas" className="max-w-full max-h-full object-contain"></canvas>
             </div>
-          );
-        },
-        fillerComponent: () => {
-          return (
-            <div className="flex-1 flex h-full overflow-hidden bg-[var(--filler-background)] p-4">
-              <p className=''>This is a filler panel.</p>
-            </div>
-          );
-        },
+          </div>
+        ),
+        fillerComponent: () => (
+          <div className="flex-1 flex h-full overflow-hidden p-4">
+            <p>This is a filler panel.</p>
+          </div>
+        ),
       }}
-      className="dockview h-full w-full"
+      className="dockview-theme-replit h-full w-full"
     />
   );
 };
