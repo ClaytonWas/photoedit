a2b8ae1a9cf8f1c7bd1c9248080314d03c33122a
ClaytonWas <claywasmund@gmail.com>
2025-11-24 10:15:24 -0500

    updated packages and removed serverless files

--- serverless.js
+++
@@ -1,6 +0,0 @@
-let imageEditor = new ImageEditor('./images/IbisPaintedEdges.jpeg', 'Ibis', 'image/jpeg', 'jpeg')
-
-imageEditor.defaultInterpolation(500, 500)
-imageEditor.changeCanvasHSV(200, 100, 100)
-
-imageEditor.quickExport()
